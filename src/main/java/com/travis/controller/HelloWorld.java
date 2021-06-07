64a288a7051a7769cb4401714e9faa6f962b9653
Seu nome <you@example.com>
2021-06-07 19:03:06 -0300

    push

--- src/main/java/com/travis/controller/HelloWorld.java
+++ src/main/java/com/travis/controller/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello-world")
+@RequestMapping("Ola-enfermeira")
 public class HelloWorld {
 
 	@GetMapping
